e523656d0209c45265e22bf303e5035b9412b54d
Ryan Cao <ryan@moduluslabs.xyz>
2023-08-25 08:01:04 -0700

    Refactor complete

---
+++ remainder_prover/src/lib.rs
@@ -0,0 +1,11 @@
+#![warn(missing_docs)]
+#![feature(closure_lifetime_binder)]
+//!Remainder: A fast GKR based library for building zkSNARKS for ML applications
+
+pub mod expression;
+pub mod layer;
+pub mod mle;
+pub mod prover;
+pub mod sumcheck;
+pub mod zkdt;
+pub mod utils;
