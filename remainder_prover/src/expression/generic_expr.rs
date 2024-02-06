add59aa0dafaa8b286812a57d6e9863bcc7e44e3
ende <endeshen99@gmail.com>
2024-02-06 15:15:23 -0800

    implementation--R: MleVecIndex

--- remainder_prover/src/expression/generic_expr.rs
+++ remainder_prover/src/expression/generic_expr.rs
@@ -1,5 +1,5 @@
 use std::{
-    fmt::Debug, marker::PhantomData, ops::{Add, Mul, Neg, Sub}
+    fmt::Debug, ops::{Add, Mul, Neg, Sub}
 };
 use serde::{Deserialize, Serialize};
 use crate::mle::MleIndex;
@@ -17,13 +17,13 @@ pub trait ExpressionType<F: FieldExt>: Serialize + for<'de> Deserialize<'de> {
 
     /// MleRefs is the optional data array of mle_refs
     /// that can be indexed into by the MleRefIndex defind in the ProverExpression
-    type MleRefs: Serialize + for<'de> Deserialize<'de>; // -- this is either unit type or Vec<DenseMleRef>
+    type MleVec: Serialize + for<'de> Deserialize<'de>; // -- this is either unit type or Vec<DenseMleRef>
     
     // type thing: Index / F -- this is container
 }
 
 /// Generic Expressions
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 #[serde(bound = "F: FieldExt")]
 pub enum ExpressionNode<F: FieldExt, E: ExpressionType<F>> {
     /// constant
@@ -51,12 +51,131 @@ pub enum ExpressionNode<F: FieldExt, E: ExpressionType<F>> {
 #[serde(bound = "F: FieldExt")]
 pub struct Expression<F: FieldExt, E: ExpressionType<F>> {
     expression_node: ExpressionNode<F, E>,
-    mle_refs: E::MleRefs,
+    mle_vec: E::MleVec,
+}
+
+/// generic methods shared across all types of expressions
+impl<F: FieldExt, E: ExpressionType<F>> Expression<F, E> {
+
+    /// Create a new expression 
+    pub fn new(expression_node: ExpressionNode<F, E>, mle_vec: E::MleVec) -> Self {
+        Self {
+            expression_node,
+            mle_vec,
+        }
+    }
+
+    /// get the expression node
+    pub fn expression_node(&self) -> &ExpressionNode<F, E> {
+        &self.expression_node
+    }
+
+    /// get the expression node
+    pub fn expression_node_mut(&mut self) -> &mut ExpressionNode<F, E> {
+        &mut self.expression_node
+    }
+
+    /// get the mle_vec
+    pub fn mle_vec(&self) -> &E::MleVec {
+        &self.mle_vec
+    }
+
+    /// get the mle_vec
+    pub fn mle_vec_mut(&mut self) -> &mut E::MleVec {
+        &mut self.mle_vec
+    }
+
+    /// get the expression node and mle_vec
+    pub fn deconstruct_mut(&mut self) -> (&mut ExpressionNode<F, E>, &mut E::MleVec) {
+        (&mut self.expression_node, &mut self.mle_vec)
+    }
+
+    /// get the expression node and mle_vec
+    pub fn deconstruct(self) -> (ExpressionNode<F, E>, E::MleVec) {
+        (self.expression_node, self.mle_vec)
+    }
+
+    /// needs the caller to modiler the observer_fn's depending on ExpressionType
+    /// traverse the expression tree, and applies the observer_fn to all child node
+    pub fn traverse<D>(
+        &self,
+        observer_fn: &mut impl FnMut(&ExpressionNode<F, E>, &E::MleVec) -> Result<(), D>,
+    ) -> Result<(), D> {
+        self.expression_node.traverse_node(observer_fn, &self.mle_vec)
+    }
+
+    /// similar to traverse
+    /// similar to traverse, but allows mutation of self
+    pub fn traverse_mut<D>(
+        &mut self,
+        observer_fn: &mut impl FnMut(&mut ExpressionNode<F, E>, &mut E::MleVec) -> Result<(), D>,
+    ) -> Result<(), D> {
+        self.expression_node.traverse_node_mut(observer_fn, &mut self.mle_vec)
+    }
 }
 
 /// generic methods shared across all types of expressions
 impl<F: FieldExt, E: ExpressionType<F>> ExpressionNode<F, E> {
 
+    /// needs the caller to modiler the observer_fn's depending on ExpressionType
+    /// traverse the expression tree, and applies the observer_fn to all child node
+    pub fn traverse_node<D>(
+        &self,
+        observer_fn: &mut impl FnMut(&ExpressionNode<F, E>, &E::MleVec) -> Result<(), D>,
+        mle_vec: &E::MleVec,
+    ) -> Result<(), D> {
+        observer_fn(self, mle_vec)?;
+        match self {
+            ExpressionNode::Constant(_)
+            | ExpressionNode::Mle(_)
+            | ExpressionNode::Product(_) => Ok(()),
+            ExpressionNode::Negated(exp) => exp.traverse_node(observer_fn, mle_vec),
+            ExpressionNode::Scaled(exp, _) => exp.traverse_node(observer_fn, mle_vec),
+            ExpressionNode::Selector(_, lhs, rhs) => {
+                lhs.traverse_node(observer_fn, mle_vec)?;
+                rhs.traverse_node(observer_fn, mle_vec)
+            }
+            ExpressionNode::Sum(lhs, rhs) => {
+                lhs.traverse_node(observer_fn, mle_vec)?;
+                rhs.traverse_node(observer_fn, mle_vec)
+            }
+        }
+    }
+
+    /// similar to traverse
+    /// similar to traverse, but allows mutation of self
+    pub fn traverse_node_mut<D>(
+        &mut self,
+        observer_fn: &mut impl FnMut(&mut ExpressionNode<F, E>, &mut E::MleVec) -> Result<(), D>,
+        mle_vec: &mut E::MleVec,
+    ) -> Result<(), D> {
+        // dbg!(&self);
+        observer_fn(self, mle_vec)?;
+        match self {
+            ExpressionNode::Constant(_)
+            | ExpressionNode::Mle(_)
+            | ExpressionNode::Product(_) => Ok(()),
+            ExpressionNode::Negated(exp) => exp.traverse_node_mut(observer_fn, mle_vec),
+            ExpressionNode::Scaled(exp, _) => exp.traverse_node_mut(observer_fn, mle_vec),
+            ExpressionNode::Selector(_, lhs, rhs) => {
+                lhs.traverse_node_mut(observer_fn, mle_vec)?;
+                rhs.traverse_node_mut(observer_fn, mle_vec)
+            }
+            ExpressionNode::Sum(lhs, rhs) => {
+                lhs.traverse_node_mut(observer_fn, mle_vec)?;
+                rhs.traverse_node_mut(observer_fn, mle_vec)
+            }
+        }
+    }
+}
+
+
+/// ----------------------------old stuff below------------------------------------
+
+/// generic methods shared across all types of expressions
+impl<F: FieldExt, E: ExpressionType<F>> ExpressionNode<F, E> {
+
+    /// needs the caller to modiler the observer_fn's depending on ExpressionType
     /// traverse the expression tree, and applies the observer_fn to all child node
     pub fn traverse<D>(
         &self,
@@ -80,6 +199,7 @@ impl<F: FieldExt, E: ExpressionType<F>> ExpressionNode<F, E> {
         }
     }
 
+    /// similar to traverse
     /// similar to traverse, but allows mutation of self
     pub fn traverse_mut<D>(
         &mut self,
@@ -104,11 +224,14 @@ impl<F: FieldExt, E: ExpressionType<F>> ExpressionNode<F, E> {
         }
     }
 
+    /// yup needs to be abstract / verifier / prover specific
     /// Concatenates two expressions together
     pub fn concat_expr(self, lhs: ExpressionNode<F, E>) -> ExpressionNode<F, E> {
         ExpressionNode::Selector(MleIndex::Iterated, Box::new(lhs), Box::new(self))
     }
 
+    /// well, if it's prover expression then, the MleRefs field needs to be populated
+    /// (not generic)
     /// Create a product Expression that multiplies many MLEs together
     pub fn products(product_list: Vec<E::Container>) -> Self {
         Self::Product(product_list)
@@ -116,6 +239,7 @@ impl<F: FieldExt, E: ExpressionType<F>> ExpressionNode<F, E> {
 }
 
 
+/// needs to be abstract / verifier / prover specific
 // the following implements the basic arithmetic operations for the generic expression
 impl<F: FieldExt, E: ExpressionType<F>> Neg for ExpressionNode<F, E> {
     type Output = ExpressionNode<F, E>;
@@ -124,6 +248,8 @@ impl<F: FieldExt, E: ExpressionType<F>> Neg for ExpressionNode<F, E> {
     }
 }
 
+/// what if the rhs introduces only the same expression,
+/// understanding is rn, we just duplicate
 impl<F: FieldExt, E: ExpressionType<F>> Add for ExpressionNode<F, E> {
     type Output = ExpressionNode<F, E>;
     fn add(self, rhs: ExpressionNode<F, E>) -> ExpressionNode<F, E> {
@@ -146,29 +272,30 @@ impl<F: FieldExt, E: ExpressionType<F>> Mul<F> for ExpressionNode<F, E> {
 }
 
 
-// defines how the Expressions are printed and displayed
-impl<F: std::fmt::Debug + FieldExt, C: Debug, E: ExpressionType<F, Container = C>> std::fmt::Debug for ExpressionNode<F, E> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ExpressionNode::Constant(scalar) => {
-                f.debug_tuple("Constant").field(scalar).finish()
-            }
-            ExpressionNode::Selector(index, a, b) => f
-                .debug_tuple("Selector")
-                .field(index)
-                .field(a)
-                .field(b)
-                .finish(),
-            // Skip enum variant and print query struct directly to maintain backwards compatibility.
-            ExpressionNode::Mle(_mle_ref) => {
-                f.debug_struct("Mle").field("mle_ref", _mle_ref).finish()
-            }
-            ExpressionNode::Negated(poly) => f.debug_tuple("Negated").field(poly).finish(),
-            ExpressionNode::Sum(a, b) => f.debug_tuple("Sum").field(a).field(b).finish(),
-            ExpressionNode::Product(a) => f.debug_tuple("Product").field(a).finish(),
-            ExpressionNode::Scaled(poly, scalar) => {
-                f.debug_tuple("Scaled").field(poly).field(scalar).finish()
-            }
-        }
-    }
-}
+// /// this also needs to be prover / verifier / abstract specific
+// // defines how the Expressions are printed and displayed
+// impl<F: std::fmt::Debug + FieldExt, C: Debug, E: ExpressionType<F, Container = C>> std::fmt::Debug for ExpressionNode<F, E> {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         match self {
+//             ExpressionNode::Constant(scalar) => {
+//                 f.debug_tuple("Constant").field(scalar).finish()
+//             }
+//             ExpressionNode::Selector(index, a, b) => f
+//                 .debug_tuple("Selector")
+//                 .field(index)
+//                 .field(a)
+//                 .field(b)
+//                 .finish(),
+//             // Skip enum variant and print query struct directly to maintain backwards compatibility.
+//             ExpressionNode::Mle(_mle_ref) => {
+//                 f.debug_struct("Mle").field("mle_ref", _mle_ref).finish()
+//             }
+//             ExpressionNode::Negated(poly) => f.debug_tuple("Negated").field(poly).finish(),
+//             ExpressionNode::Sum(a, b) => f.debug_tuple("Sum").field(a).field(b).finish(),
+//             ExpressionNode::Product(a) => f.debug_tuple("Product").field(a).finish(),
+//             ExpressionNode::Scaled(poly, scalar) => {
+//                 f.debug_tuple("Scaled").field(poly).field(scalar).finish()
+//             }
+//         }
+//     }
+// }
--- remainder_prover/src/expression/verifier_expr.rs
+++ remainder_prover/src/expression/verifier_expr.rs
@@ -4,7 +4,7 @@ use crate::mle::MleIndex;
 
 use remainder_shared_types::FieldExt;
 
-use super::{expr_errors::ExpressionError, generic_expr::{ExpressionNode, ExpressionType}};
+use super::{expr_errors::ExpressionError, generic_expr::{Expression, ExpressionNode, ExpressionType}};
 
 
 /// Verifier Expression
@@ -13,8 +13,75 @@ use super::{expr_errors::ExpressionError, generic_expr::{ExpressionNode, Express
 pub struct VerifierExpression;
 impl<F: FieldExt> ExpressionType<F> for VerifierExpression {
     type Container = F;
-    type MleRefs = ();
+    type MleVec = ();
 }
+
+impl<F: FieldExt> Expression<F, VerifierExpression> {
+    /// Evaluate the polynomial using the provided closures to perform the
+    /// operations.
+    #[allow(clippy::too_many_arguments)]
+    pub fn evaluate<T>(
+        &self,
+        constant: &impl Fn(F) -> T,
+        selector_column: &impl Fn(&MleIndex<F>, T, T) -> T,
+        mle_eval: &impl Fn(&<VerifierExpression as ExpressionType<F>>::Container) -> T,
+        negated: &impl Fn(T) -> T,
+        sum: &impl Fn(T, T) -> T,
+        product: &impl Fn(&[<VerifierExpression as ExpressionType<F>>::Container]) -> T,
+        scaled: &impl Fn(T, F) -> T,
+    ) -> T {
+        self.expression_node().evaluate(
+            constant,
+            selector_column,
+            mle_eval,
+            negated,
+            sum,
+            product,
+            scaled,
+        )
+    }
+
+    /// used by `evaluate_expr` to traverse the expression and simply
+    /// gather all of the evaluations, combining them as appropriate.
+    pub fn gather_combine_all_evals(
+        &self,
+    ) -> Result<F, ExpressionError> {
+        let constant = |c| Ok(c);
+        let selector_column =
+            |idx: &MleIndex<F>, lhs: Result<F, ExpressionError>, rhs: Result<F, ExpressionError>| {
+                // --- Selector bit must be bound ---
+                if let MleIndex::Bound(val, _) = idx {
+                    return Ok(*val * rhs? + (F::one() - val) * lhs?);
+                }
+                Err(ExpressionError::SelectorBitNotBoundError)
+            };
+        let mle_eval = for<'a> |mle_ref: &'a <VerifierExpression as ExpressionType<F>>::Container| -> Result<F, ExpressionError> {
+            Ok(mle_ref.clone())
+        };
+        let negated = |a: Result<F, ExpressionError>| match a {
+            Err(e) => Err(e),
+            Ok(val) => Ok(val.neg()),
+        };
+        let sum = |lhs: Result<F, ExpressionError>, rhs: Result<F, ExpressionError>| Ok(lhs? + rhs?);
+        let product = for<'a, 'b> |mle_refs: &'a [<VerifierExpression as ExpressionType<F>>::Container]| -> Result<F, ExpressionError> {
+            mle_refs.iter().try_fold(F::one(), |acc, new_mle_ref| {
+                Ok(acc * new_mle_ref.clone())
+            })
+        };
+        let scaled = |a: Result<F, ExpressionError>, scalar: F| Ok(a? * scalar);
+        self.evaluate(
+            &constant,
+            &selector_column,
+            &mle_eval,
+            &negated,
+            &sum,
+            &product,
+            &scaled,
+        )
+    }
+}
+
+/// -----------------below is the old stuff-----------------
 
 impl<F: FieldExt> ExpressionNode<F, VerifierExpression> {
     /// Evaluate the polynomial using the provided closures to perform the
