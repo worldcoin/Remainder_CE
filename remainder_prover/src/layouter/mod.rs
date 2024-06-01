5fd3222b3f68a6d765486468479b3916f6ccf16d
ende <endeshen99@gmail.com>
2024-05-31 19:53:01 -0700

    add tests that includes children nodes to toposort

--- remainder_prover/src/layouter/mod.rs
+++ remainder_prover/src/layouter/mod.rs
@@ -40,13 +40,13 @@ pub fn topo_sort<F: FieldExt>(nodes: &Vec<NodeEnum<F>>) -> Result<Vec<NodeEnum<F
     }
     let mut edges_out: HashMap<NodeId, HashSet<NodeId>> = HashMap::new();
     let mut edges_in: HashMap<NodeId, HashSet<NodeId>> = HashMap::new();
-    let mut starting_nodes = vec![];
+    let mut starting_nodes = HashSet::new();
 
     for node in nodes.iter() {
         let node_id = node.id();
         if node.sources().len() == 0 {
             let insert_node = children_to_parent_map.get(&node_id).unwrap_or(&node_id);
-            starting_nodes.push(*insert_node);
+            starting_nodes.insert(*insert_node);
         } else {
             for source in node.sources().iter() {
                 let insert_source = children_to_parent_map.get(source).unwrap_or(source);
@@ -65,6 +65,7 @@ pub fn topo_sort<F: FieldExt>(nodes: &Vec<NodeEnum<F>>) -> Result<Vec<NodeEnum<F
 
     let mut out = vec![];
 
+    let mut starting_nodes = starting_nodes.into_iter().collect::<Vec<NodeId>>();
     while starting_nodes.len() > 0 {
         let node = starting_nodes.pop().unwrap();
         out.push(nodes[id_to_index_map[&node]].clone());
@@ -86,6 +87,10 @@ pub fn topo_sort<F: FieldExt>(nodes: &Vec<NodeEnum<F>>) -> Result<Vec<NodeEnum<F
 
     if edges_in.len() != 0 || edges_out.len() != 0 {
         return Err(DAGError::DAGCycle);
+    }
+
+    for node in out.iter() {
+        println!("{:?}", node.id());
     }
 
     Ok(out)
@@ -232,7 +237,7 @@ pub mod tests {
     };
 
     #[test]
-    fn test_topo_sort_with_cycle() {
+    fn test_topo_sort_with_cycle_include_children() {
         //    input_layer
         //         /\
         //   input_shred_0        input_shred_3    ->    debug_node_0 -> debug_node_1 -> debug_node_0
@@ -338,7 +343,116 @@ pub mod tests {
     }
 
     #[test]
-    fn test_topo_sort() {
+    fn test_topo_sort_with_cycle_no_children() {
+        //    input_layer
+        //         /\
+        //   input_shred_0        input_shred_3    ->    debug_node_0 -> debug_node_1 -> debug_node_0
+        //   input_shred_1        input_shred_4
+        //   input_shred_2
+        //
+        //
+        //                                         ->   sector_0 ->   sector_1  ->output_0
+        //                                         ->   sector_2->  output_1
+        //
+        //
+        //
+        //
+        let ctx = Context::new();
+        let dummy_data = MultilinearExtension::new_zero();
+
+        // node ids: [1, 2, 3]
+        let input_shred_0 = InputShred::new(&ctx, dummy_data.clone(), None);
+        let input_shred_1 = InputShred::new(&ctx, dummy_data.clone(), None);
+        let input_shred_2 = InputShred::new(&ctx, dummy_data.clone(), None);
+        let input_shred_vec = vec![
+            input_shred_0.clone(),
+            input_shred_1.clone(),
+            input_shred_2.clone(),
+        ];
+        // node id: [4]
+        let input_layer_node = InputLayerNode::new(
+            &ctx,
+            Some(input_shred_vec),
+            InputLayerType::PublicInputLayer,
+        );
+
+        // node ids: [5, 6]
+        let input_shred_3_node = InputShred::new(&ctx, dummy_data.clone(), None);
+        let input_shred_4_node = InputShred::new(&ctx, dummy_data.clone(), None);
+
+        // for now, use debug node as the fake gate layer
+        // node id: [7]
+        let mut debug_node_0 = DebugNode::new(
+            &ctx,
+            "debug".to_string(),
+            &[&input_layer_node, &input_shred_3_node],
+        );
+
+        // node id: [8]
+        let sector_0_node = Sector::new(
+            &ctx,
+            &[&input_shred_0.clone(), &input_shred_1.clone()],
+            |ids| Expression::<Fr, AbstractExpr>::products(ids),
+            |_data| MultilinearExtension::new_zero(),
+        );
+
+        // node id: [9]
+        let sector_1_node = Sector::new(
+            &ctx,
+            &[&input_shred_2, &input_shred_4_node, &sector_0_node],
+            |ids| Expression::<Fr, AbstractExpr>::products(ids),
+            |_data| MultilinearExtension::new_zero(),
+        );
+
+        // node id: [10]
+        let output_0_node = OutputNode::new(&ctx, &sector_1_node);
+
+        // node id: [11]
+        let sector_2_node = Sector::new(
+            &ctx,
+            &[&input_shred_0.clone(), &input_shred_1.clone()],
+            |ids| Expression::<Fr, AbstractExpr>::products(ids),
+            |_data| MultilinearExtension::new_zero(),
+        );
+
+        // node id: [12]
+        let output_1_node = OutputNode::new(&ctx, &sector_2_node);
+
+        // adding a cycle here
+        // node id: [13]
+        let debug_node_1 = DebugNode::new(
+            &ctx,
+            "debug".to_string(),
+            &[&input_layer_node, &debug_node_0],
+        );
+
+        debug_node_0.add_node(&debug_node_1);
+
+        let mut nodes = vec![
+            NodeEnum::InputLayer(input_layer_node),
+            NodeEnum::InputShred(input_shred_3_node),
+            NodeEnum::InputShred(input_shred_4_node),
+            NodeEnum::Debug(debug_node_0),
+            NodeEnum::Debug(debug_node_1),
+            NodeEnum::Sector(sector_0_node),
+            NodeEnum::Sector(sector_1_node),
+            NodeEnum::Output(output_0_node),
+            NodeEnum::Sector(sector_2_node),
+            NodeEnum::Output(output_1_node),
+            NodeEnum::InputShred(input_shred_0),
+            NodeEnum::InputShred(input_shred_1),
+            NodeEnum::InputShred(input_shred_2),
+        ];
+
+        nodes.reverse();
+
+        let out = topo_sort(&nodes);
+
+        assert!(matches!(out, Err(DAGError::DAGCycle)))
+    }
+
+    #[test]
+    fn test_topo_sort_without_cycle_no_children() {
         //    input_layer
         //         /\
         //   input_shred_0        input_shred_3        debug_node     sector_0    sector_1->output_0
@@ -423,6 +537,123 @@ pub mod tests {
             NodeEnum::Output(output_0_node),
             NodeEnum::Sector(sector_2_node),
             NodeEnum::Output(output_1_node),
+        ];
+
+        nodes.reverse();
+
+        let out = topo_sort(&nodes).unwrap();
+
+        let mut children_to_parent_map: HashMap<NodeId, NodeId> = HashMap::new();
+        let mut id_to_index_map: HashMap<NodeId, usize> = HashMap::new();
+        for (idx, node) in out.iter().enumerate() {
+            id_to_index_map.insert(node.id(), idx);
+            if let Some(children) = node.children() {
+                for child in children.into_iter() {
+                    children_to_parent_map.insert(child, node.id());
+                }
+            }
+        }
+
+        for node in out.iter() {
+            for node_source in node.sources().iter() {
+                let node_source_parent = children_to_parent_map
+                    .get(node_source)
+                    .unwrap_or(node_source);
+
+                assert!(id_to_index_map[node_source_parent] < id_to_index_map[&node.id()]);
+            }
+        }
+    }
+
+    #[test]
+    fn test_topo_sort_without_cycle_include_children() {
+        //    input_layer
+        //         /\
+        //   input_shred_0        input_shred_3        debug_node     sector_0    sector_1->output_0
+        //   input_shred_1        input_shred_4                       sector_2->output_1
+        //   input_shred_2
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        let ctx = Context::new();
+        let dummy_data = MultilinearExtension::new_zero();
+
+        // node ids: [1, 2, 3]
+        let input_shred_0 = InputShred::new(&ctx, dummy_data.clone(), None);
+        let input_shred_1 = InputShred::new(&ctx, dummy_data.clone(), None);
+        let input_shred_2 = InputShred::new(&ctx, dummy_data.clone(), None);
+        let input_shred_vec = vec![
+            input_shred_0.clone(),
+            input_shred_1.clone(),
+            input_shred_2.clone(),
+        ];
+        // node id: [4]
+        let input_layer_node = InputLayerNode::new(
+            &ctx,
+            Some(input_shred_vec),
+            InputLayerType::PublicInputLayer,
+        );
+
+        // node ids: [5, 6]
+        let input_shred_3_node = InputShred::new(&ctx, dummy_data.clone(), None);
+        let input_shred_4_node = InputShred::new(&ctx, dummy_data.clone(), None);
+
+        // for now, use debug node as the fake gate layer
+        // node id: [7]
+        let debug_node = DebugNode::new(
+            &ctx,
+            "debug".to_string(),
+            &[&input_layer_node, &input_shred_3_node],
+        );
+
+        // node id: [8]
+        let sector_0_node = Sector::new(
+            &ctx,
+            &[&input_shred_0.clone(), &input_shred_1.clone()],
+            |ids| Expression::<Fr, AbstractExpr>::products(ids),
+            |_data| MultilinearExtension::new_zero(),
+        );
+
+        // node id: [9]
+        let sector_1_node = Sector::new(
+            &ctx,
+            &[&input_shred_2, &input_shred_4_node, &sector_0_node],
+            |ids| Expression::<Fr, AbstractExpr>::products(ids),
+            |_data| MultilinearExtension::new_zero(),
+        );
+
+        // node id: [10]
+        let output_0_node = OutputNode::new(&ctx, &sector_1_node);
+
+        // node id: [11]
+        let sector_2_node = Sector::new(
+            &ctx,
+            &[&input_shred_0.clone(), &input_shred_1.clone()],
+            |ids| Expression::<Fr, AbstractExpr>::products(ids),
+            |_data| MultilinearExtension::new_zero(),
+        );
+
+        // node id: [12]
+        let output_1_node = OutputNode::new(&ctx, &sector_2_node);
+
+        let mut nodes = vec![
+            NodeEnum::InputLayer(input_layer_node),
+            NodeEnum::InputShred(input_shred_3_node),
+            NodeEnum::InputShred(input_shred_4_node),
+            NodeEnum::Debug(debug_node),
+            NodeEnum::Sector(sector_0_node),
+            NodeEnum::Sector(sector_1_node),
+            NodeEnum::Output(output_0_node),
+            NodeEnum::Sector(sector_2_node),
+            NodeEnum::Output(output_1_node),
+            NodeEnum::InputShred(input_shred_0),
+            NodeEnum::InputShred(input_shred_1),
+            NodeEnum::InputShred(input_shred_2),
         ];
 
         nodes.reverse();
