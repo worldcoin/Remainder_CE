9fff068190672ec329ba73b800a679fdad901480
Ben Wilson <benjamin@building-babylon.net>
2024-08-29 09:01:29 +1000

    Restore the Hyrax WC tests to working order! Move the witness data one folder up!

--- remainder_hyrax/src/hyrax_gkr/test_worldcoin.rs
+++ remainder_hyrax/src/hyrax_gkr/test_worldcoin.rs
@@ -15,6 +15,7 @@ use remainder_shared_types::{
     },
     FieldExt, Poseidon,
 };
+use remainder::worldcoin::{WC_BASE, WC_NUM_DIGITS};
 
 use crate::{
     hyrax_gkr::HyraxCircuit, pedersen::PedersenCommitter, utils::vandermonde::VandermondeInverse,
@@ -22,19 +23,17 @@ use crate::{
 type Scalar = <Bn256Point as Group>::Scalar;
 type Base = <Bn256Point as CurveExt>::Base;
 
-#[test]
-fn test_hyrax_worldcoin_tiny() {
-    let mut prover_transcript: ECTranscriptWriter<Bn256Point, PoseidonSponge<Base>> =
-        ECTranscriptWriter::new("Test small regular identity matmult circuit");
+/// Helper function that runs the Hyrax Worldcoin test against a given data set.
+fn test_hyrax_worldcoin<const BASE: u16, const NUM_DIGITS: usize>(data: WorldcoinCircuitData<Scalar, BASE, NUM_DIGITS>, num_generators: usize) {
+    let mut prover_transcript: ECTranscriptWriter<Bn256Point, PoseidonSponge<Base>> = ECTranscriptWriter::new("");
     let blinding_rng = &mut rand::thread_rng();
     let converter: &mut VandermondeInverse<Scalar> = &mut VandermondeInverse::new();
-    const NUM_GENERATORS: usize = 30;
     let committer = PedersenCommitter::<Bn256Point>::new(
-        NUM_GENERATORS + 1,
+        num_generators + 1,
         "hi why is this not working, please help me",
         None,
     );
-    let data = tiny_worldcoin_data::<Scalar>();
+
     let mut circuit = build_circuit(data);
 
     let (hyrax_proof, input_commits, circuit_description) = HyraxCircuit::prove_gkr_circuit(
@@ -58,81 +57,31 @@ fn test_hyrax_worldcoin_tiny() {
         &committer,
         &mut verifier_transcript,
     );
+}
+
+#[test]
+fn test_hyrax_worldcoin_tiny() {
+    let data = tiny_worldcoin_data::<Scalar>();
+    test_hyrax_worldcoin(data, 30);
 }
 
 #[test]
 fn test_hyrax_worldcoin_medium() {
-    let mut prover_transcript: ECTranscriptWriter<Bn256Point, PoseidonSponge<Base>> =
-        ECTranscriptWriter::new("Test small regular identity matmult circuit");
-    let blinding_rng = &mut rand::thread_rng();
-    let converter: &mut VandermondeInverse<Scalar> = &mut VandermondeInverse::new();
-    const NUM_GENERATORS: usize = 30;
-    let committer = PedersenCommitter::<Bn256Point>::new(
-        NUM_GENERATORS + 1,
-        "hi why is this not working, please help me",
-        None,
-    );
     let data = medium_worldcoin_data::<Scalar>();
-    let mut circuit = build_circuit(data);
-
-    let (hyrax_proof, input_commits, circuit_description) = HyraxCircuit::prove_gkr_circuit(
-        &mut circuit,
-        &committer,
-        None,
-        None,
-        None,
-        blinding_rng,
-        converter,
-        &mut prover_transcript,
-    );
-
-    let mut verifier_transcript: ECTranscriptReader<Bn256Point, PoseidonSponge<Base>> =
-        ECTranscriptReader::new(prover_transcript.get_transcript());
-
-    HyraxCircuit::verify_gkr_circuit(
-        hyrax_proof,
-        input_commits,
-        &circuit_description,
-        &committer,
-        &mut verifier_transcript,
-    );
+    test_hyrax_worldcoin(data, 30);
 }
 
 #[test]
-fn test_hyrax_worldcoin_full() {
-    let mut prover_transcript: ECTranscriptWriter<Bn256Point, PoseidonSponge<Base>> =
-        ECTranscriptWriter::new("Test small regular identity matmult circuit");
-    let blinding_rng = &mut rand::thread_rng();
-    let converter: &mut VandermondeInverse<Scalar> = &mut VandermondeInverse::new();
-    const NUM_GENERATORS: usize = 100;
-    let committer = PedersenCommitter::<Bn256Point>::new(
-        NUM_GENERATORS + 1,
-        "hi why is this not working, please help me",
-        None,
-    );
-    let data: WorldcoinCircuitData<Scalar> =
-        load_data(Path::new("worldcoin_witness_data").to_path_buf());
-    let mut circuit = build_circuit(data);
-
-    let (hyrax_proof, input_commits, circuit_description) = HyraxCircuit::prove_gkr_circuit(
-        &mut circuit,
-        &committer,
-        None,
-        None,
-        None,
-        blinding_rng,
-        converter,
-        &mut prover_transcript,
-    );
-
-    let mut verifier_transcript: ECTranscriptReader<Bn256Point, PoseidonSponge<Base>> =
-        ECTranscriptReader::new(prover_transcript.get_transcript());
-
-    HyraxCircuit::verify_gkr_circuit(
-        hyrax_proof,
-        input_commits,
-        &circuit_description,
-        &committer,
-        &mut verifier_transcript,
-    );
+fn test_hyrax_worldcoin_v2_iris() {
+    let data: WorldcoinCircuitData<Scalar, WC_BASE, WC_NUM_DIGITS> =
+        load_data(Path::new("../worldcoin_witness_data").to_path_buf(), false);
+    test_hyrax_worldcoin(data, 100);
 }
+
+#[test]
+fn test_hyrax_worldcoin_v2_mask() {
+    let data: WorldcoinCircuitData<Scalar, WC_BASE, WC_NUM_DIGITS> =
+        load_data(Path::new("../worldcoin_witness_data").to_path_buf(), true);
+    test_hyrax_worldcoin(data, 100);
+}
+
