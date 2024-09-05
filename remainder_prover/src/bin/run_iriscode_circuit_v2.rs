e6b46eac9cd70df2aa5cf70e2651cd6fde3f842b
Ben Wilson <benjamin@building-babylon.net>
2024-09-05 16:56:07 +1000

    Restore the remainder iriscode v2 binary

--- remainder_prover/src/bin/run_iriscode_circuit_v2.rs
+++ remainder_prover/src/bin/run_iriscode_circuit_v2.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 
 use clap::Parser;
-use remainder::{prover::helpers::test_circuit, worldcoin::{circuits::build_circuit, data::{load_data, WorldcoinCircuitData}, parameters_v2::{CONSTANT_DATA_FOLDER, BASE, NUM_DIGITS}}};
+use remainder::{prover::helpers::test_circuit, worldcoin::{circuits::build_circuit, data::{load_worldcoin_data, CircuitData}, parameters_v2::{CONSTANT_DATA_FOLDER, MATMULT_NUM_ROWS, MATMULT_NUM_COLS, MATMULT_INTERNAL_DIM, BASE, NUM_DIGITS}}};
 use remainder_shared_types::Fr;
 
 #[derive(Parser, Debug)]
@@ -21,8 +21,7 @@ fn main() {
     let args = Args::parse();
     let path = Path::new("worldcoin/v2/").to_path_buf();
     let image_path = Path::new(&args.image_filepath).to_path_buf();
-    // FIXME
-    // let data: WorldcoinCircuitData<Fr, WC_BASE, WC_NUM_DIGITS> = load_data(path, image_path, args.is_mask);
-    // let circuit = build_circuit(data);
-    // test_circuit(circuit, None);
+    let data = load_worldcoin_data::<Fr, MATMULT_NUM_ROWS, MATMULT_NUM_COLS, MATMULT_INTERNAL_DIM, BASE, NUM_DIGITS>(path.clone(), image_path, args.is_mask);
+    let circuit = build_circuit(data);
+    test_circuit(circuit, None);
 }
