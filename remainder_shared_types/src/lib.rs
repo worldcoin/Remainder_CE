addf1e3853405d184dbe387f0e42d1c9e7d5ebac
ende <endeshen99@gmail.com>
2024-09-12 16:20:13 -0700

    add more detailed comments on FieldExt

--- remainder_shared_types/src/lib.rs
+++ remainder_shared_types/src/lib.rs
@@ -4,7 +4,7 @@ pub mod transcript;
 
 use std::hash::Hash;
 
-use halo2curves::ff::{Field, FromUniformBytes, PrimeField, WithSmallOrderMulGroup};
+use halo2curves::ff::{Field, FromUniformBytes, WithSmallOrderMulGroup};
 use serde::{Deserialize, Serialize};
 
 pub use halo2curves;
@@ -13,10 +13,17 @@ pub use poseidon::Poseidon;
 
 ///External definition of Field element trait, will remain an Alias for now
 pub trait FieldExt:
-    PrimeField
-    + Field
-    + FromUniformBytes<64>
-    + WithSmallOrderMulGroup<3>
+    Field
+    + FromUniformBytes<64> // only need this bc of Poseidon transcript,
+                              // see func `next_field_element_without_rejection`
+
+    + WithSmallOrderMulGroup<3> // only need this bc of halo2_fft,
+                                   // EvaluationDomain<F> uses ZETA to compute the
+                                   // `EvaluationDomain`
+
+                                   // These two traits will ideally be removed, bc
+                                   // they actually are a sub trait of `PrimeField`
+                                   // which sumcheck does not need
     + Hash
     + Ord
     + Serialize
@@ -26,8 +33,7 @@ pub trait FieldExt:
 }
 
 impl<
-        F: PrimeField
-            + Field
+        F: Field
             + FromUniformBytes<64>
             + WithSmallOrderMulGroup<3>
             + Hash
