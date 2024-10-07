a7fbd579a6669f2e91ee2770390ae8ab3a989583
Ryan Cao <ryan@moduluslabs.xyz>
2024-10-07 14:02:12 -0700

    Actually correctly randomly sampling from BN256 curve points now

--- remainder_shared_types/benches/transcript_bench.rs
+++ remainder_shared_types/benches/transcript_bench.rs
@@ -9,6 +9,7 @@
 
 use ark_std::test_rng;
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use itertools::Itertools;
 use rand::Rng;
 use remainder_shared_types::curves::PrimeOrderCurve;
 use remainder_shared_types::transcript::ec_transcript::ECProverTranscript;
@@ -29,6 +30,14 @@ type Bn256Base = <Bn256Point as PrimeOrderCurve>::Base;
 /// Literally creates a vector of uniformly random field elements.
 fn create_random_field_vec<F: Field>(num_items: usize, mut rng: impl Rng) -> Vec<F> {
     (0..num_items).map(|_| F::random(&mut rng)).collect()
+}
+
+/// Creates a vector of uniformly random BN256 G1 points.
+fn create_random_bn254_g1_vec(num_items: usize, mut rng: impl Rng) -> Vec<Bn256Point> {
+    let ret = (0..num_items)
+        .map(|_| <Bn256Point as PrimeOrderCurve>::random(&mut rng))
+        .collect_vec();
+    ret
 }
 
 // ========================================
@@ -111,7 +120,6 @@ fn bench_bn254_base_field_transcript_absorb_scalar(c: &mut Criterion) {
                         )
                     },
                     |(to_be_absorbed, mut transcript_writer)| {
-                        // TODO(ryancao): I don't know why the straightforward function call doesn't work
                         <ECTranscriptWriter<
                             Bn256Point,
                             PoseidonSponge<Bn256Base>,
@@ -162,11 +170,85 @@ fn bench_bn254_base_field_transcript_squeeze_scalar(c: &mut Criterion) {
     }
 }
 
+/// BenchmarksIDs:
+///  * `bn254_base_field_transcript_absorb_ec_{1, 10, 100, 1000, 10000}`
+///
+/// Benchmarks the [ECTranscriptWriter::append_ec_points()] functionality for
+/// a Poseidon sponge over the base field of BN-254 absorbing G1 points.
+///
+/// Note that each G1 point is equivalent to absorbing 2 base field elements.
+fn bench_bn254_base_field_transcript_absorb_ec(c: &mut Criterion) {
+    for num_elems in [1, 10, 100, 1000, 10000] {
+        c.bench_function(
+            &format!("bn254_base_field_transcript_absorb_ec_{num_elems}"),
+            |b| {
+                b.iter_batched(
+                    || {
+                        let rng = test_rng();
+                        (
+                            create_random_bn254_g1_vec(num_elems, rng),
+                            ECTranscriptWriter::<Bn256Point, PoseidonSponge<Bn256Base>>::new(
+                                "Test transcript",
+                            ),
+                        )
+                    },
+                    |(to_be_absorbed, mut transcript_writer)| {
+                        <ECTranscriptWriter<
+                            Bn256Point,
+                            PoseidonSponge<Bn256Base>,
+                        > as ECProverTranscript<Bn256Point>>::append_ec_points(
+                            &mut transcript_writer,
+                            "benchmark",
+                            &to_be_absorbed,
+                        );
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+    }
+}
+
+/// BenchmarksIDs:
+///  * `bn254_base_field_transcript_squeeze_ec_{1, 10, 100, 1000, 10000}`
+///
+/// Benchmarks the [TranscriptWriter::get_scalar_field_challenges()] functionality for
+/// a Poseidon sponge over the base field of BN-254 squeezing EC challenge points
+fn bench_bn254_base_field_transcript_squeeze_ec(c: &mut Criterion) {
+    for num_elems in [1, 10, 100, 1000, 10000] {
+        c.bench_function(
+            &format!("bn254_base_field_transcript_squeeze_ec_{num_elems}"),
+            |b| {
+                b.iter_batched(
+                    || {
+                        ECTranscriptWriter::<Bn256Point, PoseidonSponge<Bn256Base>>::new(
+                            "Test transcript",
+                        )
+                    },
+                    |mut transcript_writer| {
+                        <ECTranscriptWriter<
+                            Bn256Point,
+                            PoseidonSponge<Bn256Base>,
+                        > as ECProverTranscript<Bn256Point>>::get_ec_challenges(
+                            &mut transcript_writer,
+                            "benchmark",
+                            num_elems,
+                        )
+                    },
+                    BatchSize::SmallInput,
+                )
+            },
+        );
+    }
+}
+
 criterion_group!(
     benches,
     bench_bn254_scalar_field_transcript_absorb,
     bench_bn254_scalar_field_transcript_squeeze,
     bench_bn254_base_field_transcript_absorb_scalar,
-    bench_bn254_base_field_transcript_squeeze_scalar
+    bench_bn254_base_field_transcript_squeeze_scalar,
+    bench_bn254_base_field_transcript_absorb_ec,
+    bench_bn254_base_field_transcript_squeeze_ec,
 );
 criterion_main!(benches);
