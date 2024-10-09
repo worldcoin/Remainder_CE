7aaffabfc2dc279297b07c2343223a7623fdfbf1
Ben Wilson <ben@moduluslabs.xyz>
2024-10-09 20:04:18 +1100

    Remove InputLayerDescriptionEnum

--- remainder_prover/src/input_layer/enum_input_layer.rs
+++ remainder_prover/src/input_layer/enum_input_layer.rs
@@ -1,117 +1,18 @@
 //! A wrapper type that makes working with variants of InputLayer easier.
 
-use remainder_shared_types::{transcript::VerifierTranscript, Field};
-use serde::{Deserialize, Serialize};
+use remainder_shared_types::Field;
 
 use crate::{
     claims::wlx_eval::YieldWLXEvals,
     input_layer_enum,
-    layer::LayerId,
-    mle::{dense::DenseMle, evals::MultilinearExtension},
+    mle::dense::DenseMle,
 };
 
 use super::{
-    ligero_input_layer::{LigeroInputLayer, LigeroInputLayerDescription},
-    public_input_layer::{PublicInputLayer, PublicInputLayerDescription},
-    CommitmentEnum, InputLayerTrait, InputLayerDescriptionTrait, InputLayerError,
+    ligero_input_layer::LigeroInputLayer,
+    public_input_layer::PublicInputLayer, InputLayerTrait
 };
 
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(bound = "F: Field")]
-/// An enum representing the different types of descriptions for each layer,
-/// each description containing the shape information of the corresponding layer.
-pub enum InputLayerDescriptionEnum<F: Field> {
-    /// The circuit description for a public input layer.
-    PublicInputLayer(PublicInputLayerDescription<F>),
-    /// The circuit description for a ligero input layer.
-    LigeroInputLayer(LigeroInputLayerDescription<F>),
-}
-
-impl<F: Field> InputLayerDescriptionTrait<F> for InputLayerDescriptionEnum<F> {
-    type Commitment = InputLayerEnumVerifierCommitment<F>;
-
-    fn layer_id(&self) -> LayerId {
-        match self {
-            InputLayerDescriptionEnum::PublicInputLayer(circuit_public_input_layer) => {
-                circuit_public_input_layer.layer_id()
-            }
-            InputLayerDescriptionEnum::LigeroInputLayer(circuit_ligero_input_layer) => {
-                circuit_ligero_input_layer.layer_id()
-            }
-        }
-    }
-
-    fn get_commitment_from_transcript(
-        &self,
-        transcript_reader: &mut impl VerifierTranscript<F>,
-    ) -> Result<Self::Commitment, InputLayerError> {
-        match self {
-            InputLayerDescriptionEnum::PublicInputLayer(circuit_public_input_layer) => {
-                Ok(InputLayerEnumVerifierCommitment::PublicInputLayer(
-                    circuit_public_input_layer
-                        .get_commitment_from_transcript(transcript_reader)
-                        .unwrap(),
-                ))
-            }
-            InputLayerDescriptionEnum::LigeroInputLayer(circuit_ligero_input_layer) => {
-                Ok(InputLayerEnumVerifierCommitment::LigeroInputLayer(
-                    circuit_ligero_input_layer
-                        .get_commitment_from_transcript(transcript_reader)
-                        .unwrap(),
-                ))
-            }
-        }
-    }
-
-    fn convert_into_prover_input_layer(
-        &self,
-        mle: MultilinearExtension<F>,
-        precommit: &Option<CommitmentEnum<F>>,
-    ) -> InputLayerEnum<F> {
-        match self {
-            InputLayerDescriptionEnum::PublicInputLayer(circuit_public_input_layer) => {
-                circuit_public_input_layer.convert_into_prover_input_layer(mle, precommit)
-            }
-            InputLayerDescriptionEnum::LigeroInputLayer(circuit_ligero_input_layer) => {
-                circuit_ligero_input_layer.convert_into_prover_input_layer(mle, precommit)
-            }
-        }
-    }
-
-    fn verify(
-        &self,
-        commitment: &Self::Commitment,
-        claim: crate::claims::Claim<F>,
-        transcript_reader: &mut impl remainder_shared_types::transcript::VerifierTranscript<F>,
-    ) -> Result<(), super::InputLayerError> {
-        match self {
-            InputLayerDescriptionEnum::PublicInputLayer(circuit_public_input_layer) => {
-                match commitment {
-                    InputLayerEnumVerifierCommitment::PublicInputLayer(public_commitment) => {
-                        circuit_public_input_layer.verify(
-                            public_commitment,
-                            claim,
-                            transcript_reader,
-                        )
-                    }
-                    _ => panic!("wrong commitment type for input layer description!"),
-                }
-            }
-            InputLayerDescriptionEnum::LigeroInputLayer(circuit_ligero_input_layer) => {
-                match commitment {
-                    InputLayerEnumVerifierCommitment::LigeroInputLayer(ligero_commitment) => {
-                        circuit_ligero_input_layer.verify(
-                            ligero_commitment,
-                            claim,
-                            transcript_reader,
-                        )
-                    }
-                    _ => panic!("wrong commitment type for input layer description!"),
-                }
-            }
-        }
-    }
-}
 
 input_layer_enum!(
     InputLayerEnum,
