ab6d65cdabbd93c63b0b2cd3540b384330fdf463
Makis Arsenis <makis@moduluslabs.xyz>
2024-10-11 01:01:16 +0000

    Respond to PR comments

--- remainder_shared_types/src/lib.rs
+++ remainder_shared_types/src/lib.rs
@@ -5,7 +5,6 @@ pub mod transcript;
 use std::hash::Hash;
 
 use halo2curves::ff::{FromUniformBytes, WithSmallOrderMulGroup};
-use num::{BigUint, One};
 use serde::{Deserialize, Serialize};
 
 pub use halo2curves::ff::Field as ff_field;
@@ -62,67 +61,11 @@ pub trait HasByteRepresentation {
     /// in a byte array of length `REPR_NUM_BYTES`.
     fn to_bytes_le(&self) -> Vec<u8>;
 
-    fn to_u64s_le(&self) -> Vec<u64> {
-        let bytes = self.to_bytes_le();
+    /// Similar to `to_bytes_le` but returns chunks of `u64`s.
+    fn to_u64s_le(&self) -> Vec<u64>;
 
-        let fold_bytes = |acc, x: &u8| (acc << 8) + (*x as u64);
-
-        vec![
-            bytes[0..8].iter().rev().fold(0, fold_bytes),
-            bytes[8..16].iter().rev().fold(0, fold_bytes),
-            bytes[16..24].iter().rev().fold(0, fold_bytes),
-            bytes[24..32].iter().rev().fold(0, fold_bytes),
-        ]
-    }
-
+    /// Similar to `from_bytes_le` but takes chunks of `u64`s.
     fn from_u64s_le(words: Vec<u64>) -> Self
     where
-        Self: Sized,
-    {
-        let mask_8bit = (1_u64 << 8) - 1;
-
-        Self::from_bytes_le(vec![
-            (words[0] & mask_8bit) as u8,
-            ((words[0] & (mask_8bit << 8)) >> 8) as u8,
-            ((words[0] & (mask_8bit << 16)) >> 16) as u8,
-            ((words[0] & (mask_8bit << 24)) >> 24) as u8,
-            ((words[0] & (mask_8bit << 32)) >> 32) as u8,
-            ((words[0] & (mask_8bit << 40)) >> 40) as u8,
-            ((words[0] & (mask_8bit << 48)) >> 48) as u8,
-            ((words[0] & (mask_8bit << 56)) >> 56) as u8,
-            (words[1] & mask_8bit) as u8,
-            ((words[1] & (mask_8bit << 8)) >> 8) as u8,
-            ((words[1] & (mask_8bit << 16)) >> 16) as u8,
-            ((words[1] & (mask_8bit << 24)) >> 24) as u8,
-            ((words[1] & (mask_8bit << 32)) >> 32) as u8,
-            ((words[1] & (mask_8bit << 40)) >> 40) as u8,
-            ((words[1] & (mask_8bit << 48)) >> 48) as u8,
-            ((words[1] & (mask_8bit << 56)) >> 56) as u8,
-            (words[2] & mask_8bit) as u8,
-            ((words[2] & (mask_8bit << 8)) >> 8) as u8,
-            ((words[2] & (mask_8bit << 16)) >> 16) as u8,
-            ((words[2] & (mask_8bit << 24)) >> 24) as u8,
-            ((words[2] & (mask_8bit << 32)) >> 32) as u8,
-            ((words[2] & (mask_8bit << 40)) >> 40) as u8,
-            ((words[2] & (mask_8bit << 48)) >> 48) as u8,
-            ((words[2] & (mask_8bit << 56)) >> 56) as u8,
-            (words[3] & mask_8bit) as u8,
-            ((words[3] & (mask_8bit << 8)) >> 8) as u8,
-            ((words[3] & (mask_8bit << 16)) >> 16) as u8,
-            ((words[3] & (mask_8bit << 24)) >> 24) as u8,
-            ((words[3] & (mask_8bit << 32)) >> 32) as u8,
-            ((words[3] & (mask_8bit << 40)) >> 40) as u8,
-            ((words[3] & (mask_8bit << 48)) >> 48) as u8,
-            ((words[3] & (mask_8bit << 56)) >> 56) as u8,
-        ])
-    }
-
-    fn to_big_uint(&self) -> BigUint {
-        self.to_bytes_le()
-            .iter()
-            .rev()
-            .fold(BigUint::ZERO, |acc, byte| {
-                acc * (BigUint::one() << 8) + *byte
-            })
-    }
+        Self: Sized;
 }
