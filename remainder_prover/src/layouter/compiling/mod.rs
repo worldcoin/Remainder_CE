310b1def01a6ce1b0e6d296b8e4814603e2f4044
Ryan Cao <ryan@moduluslabs.xyz>
2024-10-18 11:56:57 -0700

    BEGONE LAYOUTERCIRCUIT

--- remainder_prover/src/layouter/compiling/mod.rs
+++ remainder_prover/src/layouter/compiling/mod.rs
@@ -1,4 +1,3 @@
-#![allow(clippy::type_complexity)]
 //! This is a module for constructing an [InstantiatedCircuit] given a
 //! [LayouterCircuit], which is a closure provided by the circuit builder that
 //! takes in one parameter, the context, along with all of the "circuit
@@ -19,23 +18,6 @@
 #[cfg(test)]
 mod tests;
 
-use remainder_shared_types::transcript::poseidon_transcript::PoseidonSponge;
-use remainder_shared_types::Field;
-use std::collections::HashMap;
-use std::marker::PhantomData;
-
-use crate::layer::LayerId;
-use crate::mle::evals::MultilinearExtension;
-use crate::prover::{generate_circuit_description, prove, GKRCircuitDescription, GKRError};
-use remainder_shared_types::transcript::{Transcript, TranscriptWriter};
-
-use super::nodes::circuit_inputs::InputLayerNodeData;
-use super::nodes::NodeId;
-use super::{
-    component::Component,
-    nodes::{node_enum::NodeEnum, Context},
-};
-
 /// Defines the type of hash used when adding a circuit description's hash
 /// into the transcript.
 pub enum CircuitHashType {
@@ -51,91 +33,4 @@ pub enum CircuitHashType {
     /// [serde::Serialize] and hashes the bytes in chunks of 16, converting
     /// them first to field elements.
     Poseidon,
-}
-
-/// The struct used in order to aid with proving, which contains the function
-/// used in order to generate the circuit description itself, called the
-/// `witness_builder`, which can then be used to generate an
-/// [InstantiatedCircuit].
-pub struct LayouterCircuit<
-    F: Field,
-    C: Component<NodeEnum<F>>,
-    Fn: FnMut(&Context) -> (C, Vec<InputLayerNodeData<F>>),
-> {
-    witness_builder: Fn,
-    _marker: PhantomData<F>,
-}
-
-impl<
-        F: Field,
-        C: Component<NodeEnum<F>>,
-        Fn: FnMut(&Context) -> (C, Vec<InputLayerNodeData<F>>),
-    > LayouterCircuit<F, C, Fn>
-{
-    /// Constructs a `LayouterCircuit` by taking in a closure whose parameter is
-    /// a [Context], which determines the ID of each individual part of the
-    /// circuit whose data output will be computed for future layers. The
-    /// function itself returns a [Component], which is a set of circuit "nodes"
-    /// which will be used to build the circuit itself. The closure also returns
-    /// a [Vec<InputLayerNodeData>], which is a vector of all of the data that will
-    /// be fed into the input layers of the circuit.
-    pub fn new(witness_builder: Fn) -> Self {
-        Self {
-            witness_builder,
-            _marker: PhantomData,
-        }
-    }
-
-    /// From the witness builder, generate a circuit description and then an
-    /// instantiated circuit. Then, write the "proof" of this circuit to the
-    /// `transcript_writer`. Return the circuit description for verifying.
-    pub fn prove(
-        &mut self,
-        mut transcript_writer: TranscriptWriter<F, PoseidonSponge<F>>,
-    ) -> Result<
-        (
-            Transcript<F>,
-            GKRCircuitDescription<F>,
-            HashMap<LayerId, MultilinearExtension<F>>,
-        ),
-        GKRError,
-    > {
-        let ctx = Context::new();
-        let (component, input_layer_data) = (self.witness_builder)(&ctx);
-
-        // Convert the input layer data into a map that maps the input shred ID
-        // i.e. adapt witness builder output to the instantate() function.
-        // This can be removed once witness builders are removed.
-        let mut shred_id_to_data = HashMap::<NodeId, MultilinearExtension<F>>::new();
-        input_layer_data.into_iter().for_each(|input_layer_data| {
-            input_layer_data
-                .data
-                .into_iter()
-                .for_each(|input_shred_data| {
-                    shred_id_to_data.insert(
-                        input_shred_data.corresponding_input_shred_id,
-                        input_shred_data.data,
-                    );
-                });
-        });
-
-        let (circuit_description, input_builder, _) =
-            generate_circuit_description(component.yield_nodes()).unwrap();
-
-        let inputs = input_builder(shred_id_to_data).unwrap();
-        prove(
-            &inputs,
-            &HashMap::new(),
-            &circuit_description,
-            CircuitHashType::Sha3_256,
-            &mut transcript_writer,
-        )
-        .unwrap();
-
-        Ok((
-            transcript_writer.get_transcript(),
-            circuit_description,
-            inputs,
-        ))
-    }
 }
--- remainder_prover/src/worldcoin/circuits.rs
+++ remainder_prover/src/worldcoin/circuits.rs
@@ -5,9 +5,8 @@ use crate::digits::components::{
 };
 use crate::input_layer::InputLayerDescription;
 use crate::layer::LayerId;
-use crate::layouter::compiling::LayouterCircuit;
-use crate::layouter::component::{Component, ComponentSet};
-use crate::layouter::nodes::circuit_inputs::{InputLayerNode, InputLayerNodeData, InputShred};
+use crate::layouter::component::Component;
+use crate::layouter::nodes::circuit_inputs::{InputLayerNode, InputShred};
 use crate::layouter::nodes::circuit_outputs::OutputNode;
 use crate::layouter::nodes::fiat_shamir_challenge::FiatShamirChallengeNode;
 use crate::layouter::nodes::identity_gate::IdentityGateNode;
@@ -17,196 +16,12 @@ use crate::layouter::nodes::node_enum::NodeEnum;
 use crate::layouter::nodes::{CircuitNode, Context, NodeId};
 use crate::mle::evals::MultilinearExtension;
 use crate::prover::{generate_circuit_description, GKRCircuitDescription};
-use crate::utils::{build_input_shred_and_data, get_input_shred_and_data};
 use crate::worldcoin::components::Subtractor;
 
 use itertools::Itertools;
 use remainder_shared_types::Field;
 
 use super::data::IriscodeCircuitData;
-
-/// Builds the iriscode circuit.
-///
-/// This circuit is satisfied iff the (entry-wise) complementary decomposition of the result of the
-/// matrix multiplication, minus `to_sub_from_matmult`, has digits `digits` and sign bits
-/// `sign_bits`.  In more detail:
-/// + The matrix multiplication has two multiplicands.  The left-hand multiplicand is obtained by
-///   selecting the entries of `to_reroute` in the manner specified by `wirings`; the right-hand
-///   multiplicand is given by `rh_matmult_multiplicand`.  All matrix enumerations are row-major.
-/// + The complementary decomposition is the analog of 2's complement, base `BASE` and with
-///   `NUM_DIGITS` digits.  See [crate::digits::complementary_decomposition] and
-///   [Notion](https://www.notion.so/Constraining-for-the-response-zero-case-using-the-complementary-representation-d77ddfe258a74a9ab949385cc6f7eda4).
-/// + The sign bits are range checked using a degree two polynomial.  The digits are range-checked
-///   using a lookup and the multiplicities `digit_multiplicities`.
-///
-/// The structure of the circuit is determined by:
-/// + the `reroutings`, which determine how to build the MLE of left-hand multiplicand of the matrix
-///   multiplication from the MLE of the input `to_reroute`;
-/// + the generics `BASE`, `NUM_DIGITS`, `MATMULT_ROWS_NUM_VARS`, `MATMULT_COLS_NUM_VARS`,
-///   `MATMULT_INTERNAL_DIM_NUM_VARS`;
-/// + the length of the MLE `to_reroute`.
-///
-/// See [CircuitData] for a detailed description of each generic and argument with all public input layers.
-pub fn build_circuit<
-    F: Field,
-    const MATMULT_ROWS_NUM_VARS: usize,
-    const MATMULT_COLS_NUM_VARS: usize,
-    const MATMULT_INTERNAL_DIM_NUM_VARS: usize,
-    const BASE: u64,
-    const NUM_DIGITS: usize,
->(
-    data: IriscodeCircuitData<F>,
-    reroutings: Vec<(usize, usize)>,
-) -> LayouterCircuit<
-    F,
-    ComponentSet<NodeEnum<F>>,
-    impl FnMut(&Context) -> (ComponentSet<NodeEnum<F>>, Vec<InputLayerNodeData<F>>),
-> {
-    LayouterCircuit::new(move |ctx| {
-        let IriscodeCircuitData {
-            to_reroute,
-            rh_matmult_multiplicand,
-            digits,
-            sign_bits,
-            digit_multiplicities,
-            to_sub_from_matmult,
-        } = &data;
-        let mut output_nodes = vec![];
-
-        let input_layer = InputLayerNode::new(ctx, None);
-        println!("{:?} = Input layer", input_layer.id());
-        // TODO shouldn't have to clone here, but need to change library functions
-        let (to_reroute, to_reroute_data) =
-            build_input_shred_and_data(to_reroute.clone(), ctx, &input_layer);
-        println!("{:?} = Image to_reroute input", to_reroute.id());
-        let (to_sub_from_matmult, to_sub_from_matmult_data) =
-            build_input_shred_and_data(to_sub_from_matmult.clone(), ctx, &input_layer);
-        println!("{:?} = input to sub from matmult", to_sub_from_matmult.id());
-        let rerouted_image = IdentityGateNode::new(ctx, &to_reroute, reroutings.clone(), None);
-        println!("{:?} = Identity gate", rerouted_image.id());
-
-        let (rh_matmult_multiplicand, rh_matmult_multiplicand_data) =
-            build_input_shred_and_data(rh_matmult_multiplicand.clone(), ctx, &input_layer);
-        println!(
-            "{:?} = Kernel values (RH multiplicand of matmult) input",
-            rh_matmult_multiplicand.id()
-        );
-
-        let matmult = MatMultNode::new(
-            ctx,
-            &rerouted_image,
-            (MATMULT_ROWS_NUM_VARS, MATMULT_INTERNAL_DIM_NUM_VARS),
-            &rh_matmult_multiplicand,
-            (MATMULT_INTERNAL_DIM_NUM_VARS, MATMULT_COLS_NUM_VARS),
-        );
-        println!("{:?} = Matmult", matmult.id());
-
-        let subtractor = Subtractor::new(ctx, &matmult, &to_sub_from_matmult);
-
-        let (digits_input_shreds, digits_input_shreds_data): (Vec<_>, Vec<_>) = digits
-            .iter()
-            .map(|digit_values| build_input_shred_and_data(digit_values.clone(), ctx, &input_layer))
-            .unzip();
-        for (i, shred) in digits_input_shreds.iter().enumerate() {
-            println!("{:?} = {}th digit input", shred.id(), i);
-        }
-        let digits_refs = digits_input_shreds
-            .iter()
-            .map(|shred| shred as &dyn CircuitNode)
-            .collect_vec();
-        // Concatenate the digits (which are stored for each digital place separately) into a single
-        // MLE
-        let digits_concatenator = DigitsConcatenator::new(ctx, &digits_refs);
-
-        // Use a lookup to range check the digits to the range 0..BASE
-        let (lookup_table_values, lookup_table_values_data) =
-            get_input_shred_and_data((0..BASE).map(F::from).collect(), ctx, &input_layer);
-        println!("{:?} = Digit range check input", lookup_table_values.id());
-
-        let fiat_shamir_challenge_node = FiatShamirChallengeNode::new(ctx, 1);
-        let lookup_table =
-            LookupTable::new::<F>(ctx, &lookup_table_values, &fiat_shamir_challenge_node);
-        println!("{:?} = Lookup table", lookup_table.id());
-        let (digit_multiplicities, digit_multiplicities_data) =
-            build_input_shred_and_data(digit_multiplicities.clone(), ctx, &input_layer);
-        println!("{:?} = Digit multiplicities", digit_multiplicities.id());
-        let lookup_constraint = LookupConstraint::new::<F>(
-            ctx,
-            &lookup_table,
-            &&digits_concatenator.sector,
-            &digit_multiplicities,
-        );
-        println!("{:?} = Lookup constraint", lookup_constraint.id());
-
-        let unsigned_recomp = UnsignedRecomposition::new(ctx, &digits_refs, BASE);
-
-        let (sign_bits, sign_bits_data) =
-            build_input_shred_and_data(sign_bits.clone(), ctx, &input_layer);
-        println!("{:?} = Sign bits (iris code) input", sign_bits.id());
-        let complementary_checker = ComplementaryRecompChecker::new(
-            ctx,
-            &&subtractor.sector,
-            &sign_bits,
-            &&unsigned_recomp.sector,
-            BASE,
-            NUM_DIGITS,
-        );
-        output_nodes.push(OutputNode::new_zero(ctx, &complementary_checker.sector));
-
-        let bits_are_binary = BitsAreBinary::new(ctx, &sign_bits);
-        output_nodes.push(OutputNode::new_zero(ctx, &bits_are_binary.sector));
-
-        let mut input_data_shreds = vec![
-            to_reroute_data,
-            rh_matmult_multiplicand_data,
-            sign_bits_data,
-            to_sub_from_matmult_data,
-            digit_multiplicities_data,
-            lookup_table_values_data,
-        ];
-        input_data_shreds.extend(digits_input_shreds_data);
-        let input_layer_data = InputLayerNodeData::new(input_layer.id(), input_data_shreds);
-
-        // Collect all the nodes, starting with the input nodes
-        let mut all_nodes: Vec<NodeEnum<F>> = vec![
-            input_layer.into(),
-            // FIXME restore old order
-            lookup_table_values.into(),
-            fiat_shamir_challenge_node.into(),
-            to_reroute.into(),
-            rh_matmult_multiplicand.into(),
-            sign_bits.into(),
-            to_sub_from_matmult.into(),
-            digit_multiplicities.into(),
-        ];
-        all_nodes.extend(digits_input_shreds.into_iter().map(|node| node.into()));
-
-        // Add the identity gate node
-        all_nodes.push(rerouted_image.into());
-
-        // Add matmult node
-        all_nodes.push(matmult.into());
-
-        // Add the lookup nodes
-        all_nodes.push(lookup_table.into());
-        all_nodes.push(lookup_constraint.into());
-
-        // Add nodes from components
-        all_nodes.extend(subtractor.yield_nodes());
-        all_nodes.extend(digits_concatenator.yield_nodes());
-        all_nodes.extend(bits_are_binary.yield_nodes());
-        all_nodes.extend(unsigned_recomp.yield_nodes());
-        all_nodes.extend(complementary_checker.yield_nodes());
-
-        // Add output nodes
-        all_nodes.extend(output_nodes.into_iter().map(|node| node.into()));
-
-        (
-            ComponentSet::<NodeEnum<F>>::new_raw(all_nodes),
-            vec![input_layer_data],
-        )
-    })
-}
 
 /// Builds the iriscode circuit, return the circuit description, the input node map and the input
 /// layer ids of the input layers for the public and private inputs.
