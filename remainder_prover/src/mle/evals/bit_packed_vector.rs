78c54aa6d65959afe1cb8ca7c686598afad84b2a
vishady <vishruti721@gmail.com>
2024-10-31 15:20:19 -0700

    keep flag true

--- remainder_prover/src/mle/evals/bit_packed_vector.rs
+++ remainder_prover/src/mle/evals/bit_packed_vector.rs
@@ -16,7 +16,7 @@ use itertools::FoldWhile::{Continue, Done};
 /// `F`, effectively behaving like a regular (immutable) `Vec<F>`. This is
 /// needed because bit-packing incurs a noticable runtime slowdown, and we need
 /// an easy way to turn it off if trading memory for speed is desirable.
-const ENABLE_BIT_PACKING: bool = false;
+const ENABLE_BIT_PACKING: bool = true;
 
 // -------------- Helper Functions -----------------
 
