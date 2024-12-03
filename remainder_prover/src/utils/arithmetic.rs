6d280aba87ce7c5ad4dd690ed1bd1ee1ac32cc9c
vishady <vishruti721@gmail.com>
2024-12-02 17:15:13 -0800

    address comment

--- remainder_prover/src/utils/arithmetic.rs
+++ remainder_prover/src/utils/arithmetic.rs
@@ -1,3 +1,5 @@
+use funty::Unsigned;
+use num::Zero;
 use remainder_shared_types::Field;
 
 /// Helper function for conversion to field elements, handling negative values.
@@ -10,8 +12,8 @@ pub fn i64_to_field<F: Field>(value: i64) -> F {
 }
 
 /// Take the ceil(log2(value)) for a u64 value.
-pub fn log2_ceil(value: u64) -> u32 {
-    if value == 0 {
+pub fn log2_ceil<T: Unsigned + Zero>(value: T) -> u32 {
+    if value == T::zero() {
         0
     } else if value.is_power_of_two() {
         1u64.leading_zeros() - value.leading_zeros()
