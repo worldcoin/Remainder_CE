d0b5add7c123ea889d3a2d99e99fb28a5b8e6927
Kunming Jiang <kunmingj@andrew.cmu.edu>
2025-07-24 12:01:30 -0700

    WIP PostSumcheckLayerTree

--- remainder_prover/src/layer/product.rs
+++ remainder_prover/src/layer/product.rs
@@ -139,257 +139,31 @@ impl<F: Field> PostSumcheckLayerTree<F, F> {
             left: Box::new(lhs), 
             right: Box::new(rhs),
             // some intermediate addition values do not need to be recorded
-            // but we do not reason here 
+            // but we write down everything and remove later in [remove_add_values]
             value: Some(value),
         }
     }
-}
 
-/*
-/// Represents a fully bound product of MLEs, or a single MLE (which we consider a simple product).
-/// Data structure for extracting the values to be commited to, their "mxi" values (i.e. their
-/// public coefficients), and also the claims made on other layers.
-#[derive(Debug)]
-pub struct Product<F: Field, T> {
-    /// the evaluated MLEs that are being multiplied and their intermediates
-    /// in the order a, b, ab, c, abc, d, abcd, ...
-    pub intermediates: Vec<Intermediate<F, T>>,
-    /// the (public) coefficient i.e. the "mxi"
-    pub coefficient: F,
-}
-
-impl<F: Field> Product<F, Option<F>> {
-    /// Creates a new Product from a vector of [MleDescriptions].
-    pub fn new(mles: &[MleDescription<F>], coefficient: F, bindings: &[F]) -> Self {
-        if mles.is_empty() {
-            return Product {
-                intermediates: vec![Intermediate::Composite {
-                    value: Some(F::ONE),
-                }],
-                coefficient,
-            };
-        }
-        let mut intermediates = vec![Self::build_atom(&mles[0], bindings)];
-        mles.iter().skip(1).for_each(|mle| {
-            intermediates.push(Self::build_atom(mle, bindings));
-            intermediates.push(Intermediate::Composite { value: None });
-        });
-        Product {
-            intermediates,
-            coefficient,
-        }
-    }
-
-    /// Creates a new Product from a vector of [MleDescriptions].
-    pub fn new_from_mul_gate(
-        mles: &[MleDescription<F>],
-        coefficient: F,
-        bindings: &[&[F]],
-    ) -> Self {
-        if mles.is_empty() {
-            return Product {
-                intermediates: vec![Intermediate::Composite {
-                    value: Some(F::ONE),
-                }],
-                coefficient,
-            };
-        }
-        let mut intermediates = vec![Self::build_atom(&mles[0], bindings[0])];
-        mles.iter().enumerate().skip(1).for_each(|(idx, mle_ref)| {
-            intermediates.push(Self::build_atom(mle_ref, bindings[idx]));
-            intermediates.push(Intermediate::Composite { value: None });
-        });
-        Product {
-            intermediates,
-            coefficient,
-        }
-    }
-
-    // Helper function for new
-    fn build_atom(mle: &MleDescription<F>, bindings: &[F]) -> Intermediate<F, Option<F>> {
-        Intermediate::Atom {
-            layer_id: mle.layer_id(),
-            point: mle.get_claim_point(bindings),
-            value: None,
-        }
-    }
-}
-
-impl<F: Field> Product<F, F> {
-    /// Creates a new Product from a vector of fully bound MleRefs.
-    /// Panics if any are not fully bound.
-    pub fn new(mles: &[DenseMle<F>], coefficient: F) -> Self {
-        // ensure all MLEs are fully bound
-        assert!(mles.iter().all(|mle| mle.is_fully_bounded()));
-        if mles.is_empty() {
-            return Product {
-                intermediates: vec![Intermediate::Composite { value: F::ONE }],
-                coefficient,
-            };
-        }
-        let mut intermediates = vec![Self::build_atom(&mles[0])];
-        let _ = mles.iter().skip(1).fold(mles[0].value(), |acc, mle| {
-            let prod_val = acc * mle.value();
-            intermediates.push(Self::build_atom(mle));
-            intermediates.push(Intermediate::Composite { value: prod_val });
-            prod_val
-        });
-        Product {
-            intermediates,
-            coefficient,
-        }
-    }
-
-    // Helper function for new
-    fn build_atom(mle: &DenseMle<F>) -> Intermediate<F, F> {
-        Intermediate::Atom {
-            layer_id: mle.layer_id,
-            point: mle.get_bound_point(),
-            value: mle.value(),
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
-/// Represents either an atomic factor of a product (i.e. an evaluation of an MLE), or the result of
-/// an intermediate product of atoms.
-pub enum Intermediate<F: Field, T> {
-    /// A struct representing a single MLE and a commitment to its evaluation.
-    Atom {
-        /// the id of the layer upon which this is a claim
-        layer_id: LayerId,
-        /// the evaluation point
-        point: Vec<F>,
-        /// the value (C::Scalar), commitment to the value (C), or CommittedScalar
-        value: T,
-    },
-    /// A struct representing a commitment to the product of two MLE evaluations.
-    Composite {
-        /// the value, commitment to the value, or CommittedScalar
-        value: T,
-    },
-}
-
-impl<F: Field, T: Copy> PostSumcheckLayer<F, T> {
-    /// Returns a vector of the values of the intermediates (in an order compatible with [set_values]).
-    pub fn get_values(&self) -> Vec<T> {
-        self.0
-            .iter()
-            .flat_map(|product| {
-                product
-                    .intermediates
-                    .iter()
-                    .map(|pp| match pp {
-                        Intermediate::Atom { value, .. } => *value,
-                        Intermediate::Composite { value } => *value,
-                    })
-                    .collect::<Vec<T>>()
-            })
-            .collect()
-    }
-
-    /// Return a vector of all the coefficients of the products.
-    pub fn get_coefficients(&self) -> Vec<F> {
-        self.0.iter().map(|product| product.coefficient).collect()
-    }
-}
-
-/// Set the values of the PostSumcheckLayer to the given values, panicking if the lengths do not match,
-/// returning a new instance. Counterpart to [get_values].
-pub fn new_with_values<F: Field, S, T: Clone>(
-    post_sumcheck_layer: &PostSumcheckLayer<F, S>,
-    values: &[T],
-) -> PostSumcheckLayer<F, T> {
-    let total_len: usize = post_sumcheck_layer
-        .0
-        .iter()
-        .map(|product| product.intermediates.len())
-        .sum();
-    assert_eq!(total_len, values.len());
-    let mut start = 0;
-    PostSumcheckLayer(
-        post_sumcheck_layer
-            .0
-            .iter()
-            .map(|product| {
-                let end = start + product.intermediates.len();
-                let product_values = values[start..end].to_vec();
-                start = end;
-                new_with_values_single(product, product_values)
-            })
-            .collect(),
-    )
-}
-
-/// Helper for [new_with_values].
-/// Set the values of the Product to the given values, panicking if the lengths do not match.
-fn new_with_values_single<F: Field, S, T>(
-    product: &Product<F, S>,
-    values: Vec<T>,
-) -> Product<F, T> {
-    assert_eq!(product.intermediates.len(), values.len());
-    Product {
-        coefficient: product.coefficient,
-        intermediates: product
-            .intermediates
-            .iter()
-            .zip(values)
-            .map(|(pp, value)| match pp {
-                Intermediate::Atom {
-                    layer_id, point, ..
-                } => Intermediate::Atom {
-                    layer_id: *layer_id,
-                    point: point.clone(),
-                    value,
-                },
-                Intermediate::Composite { .. } => Intermediate::Composite { value },
-            })
-            .collect(),
-    }
-}
-
-impl<F: Field, T: Clone> Product<F, T> {
-    /// Return the resulting value of the product.
-    /// Useful to build the commitment to the oracle evaluation.
-    pub fn get_result(&self) -> T {
-        let last = &self.intermediates[self.intermediates.len() - 1];
-        match last {
-            Intermediate::Atom { value, .. } => {
-                // this product had better consist of just one MLE!
-                assert_eq!(self.intermediates.len(), 1);
-                value.clone()
+    /// iterate through the entire tree, remove values of the nodes if
+    /// 1. it is an addition node, AND
+    /// 2. it is not a child of a multiplication node
+    /// `mult_child` is FALSE at root
+    pub fn remove_add_values(&mut self, mult_child: bool) {
+        match self {
+            Self::Mle { layer_id: _, point: _, value: _ } => {},
+            Self::Constant { coefficient: _ } => {},
+            Self::Mult { left, right, value: _ } => {
+                left.remove_add_values(true);
+                right.remove_add_values(true);
             }
-            Intermediate::Composite { value, .. } => value.clone(),
-        }
-    }
-
-    /// Return a vector of triples (x, y, z) where z=x*y, or None.
-    pub fn get_product_triples(&self) -> Option<Vec<(T, T, T)>> {
-        if self.intermediates.len() > 1 {
-            assert!(self.intermediates.len() >= 3);
-            let values = self
-                .intermediates
-                .iter()
-                .map(|pp| match pp {
-                    Intermediate::Atom { value, .. } => value.clone(),
-                    Intermediate::Composite { value, .. } => value.clone(),
-                })
-                .collect::<Vec<_>>();
-            Some(
-                values
-                    .windows(3)
-                    .map(|window| {
-                        if let [x, y, z] = window {
-                            (x.clone(), y.clone(), z.clone())
-                        } else {
-                            unreachable!()
-                        }
-                    })
-                    .collect::<Vec<_>>(),
-            )
-        } else {
-            None
+            Self::Add { left, right, value } => {
+                left.remove_add_values(false);
+                right.remove_add_values(false);
+                // The only time we need to change something
+                if !mult_child {
+                    *value = None;
+                }
+            }
         }
     }
 }
-*/
