4940f0bf5aa63aa7a670f4648f87767ce924c5c7
Kunming Jiang <unming.jiang@toolsforhumanity.com>
2025-08-04 11:48:44 -0700

    Bug fixes

--- remainder_prover/src/expression/circuit_expr.rs
+++ remainder_prover/src/expression/circuit_expr.rs
@@ -746,8 +746,8 @@ impl<F: Field> Expression<F, ExprDescription> {
     /// Create a nested selector Expression that selects between 2^k Expressions
     /// by creating a binary tree of Selector Expressions.
     /// The order of the leaves is the order of the input expressions.
-    /// (Note that this is very different from calling `select()` consecutively.)
-    pub fn selectors(expressions: Vec<Self>) -> Self {
+    /// (Note that this is very different from calling [Self::select] consecutively.)
+    pub fn select_bin(expressions: Vec<Self>) -> Self {
         // Ensure length is a power of two
         assert!(expressions.len().is_power_of_two());
         let mut expressions = expressions;
