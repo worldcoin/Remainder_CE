3ed0ad176f20621c626550b1cca191d98f9d5c93
Ryan Cao <goldenviolin7@gmail.com>
2025-09-05 16:07:46 -0700

    Fixed bug

--- remainder_prover/tests/bug.rs
+++ remainder_prover/tests/bug.rs
@@ -16,17 +16,20 @@ fn build_circuit() -> Circuit<Fr> {
     let mut builder = CircuitBuilder::new();
 
     let input_layer = builder.add_input_layer(LayerKind::Public);
+    let primary_input_mle = builder.add_input_shred("Primary Input MLE", 3, &input_layer);
+    let left_half_shifted_expected_mle =
+        builder.add_input_shred("Left Half Shifted Expected MLE", 2, &input_layer);
+    let right_half_shifted_expected_mle =
+        builder.add_input_shred("Right Half Shifted Expected MLE", 2, &input_layer);
 
-    let all_inputs = builder.add_input_shred("All Inputs", 3, &input_layer);
-
-    let shifter = ShiftNode::new(&mut builder, 3, -1, &all_inputs);
+    let shifter = ShiftNode::new(&mut builder, 3, 1, &primary_input_mle);
 
     let shifter_splits = builder.add_split_node(&shifter.get_output(), 1);
 
-    // Dummy output node involving first split
-    let output_part_1 = builder.add_sector(shifter_splits[0].expr() - shifter_splits[0].expr());
-    // Dummy output node involving second split
-    let output_part_2 = builder.add_sector(shifter_splits[1].expr() - shifter_splits[1].expr());
+    let output_part_1 =
+        builder.add_sector(shifter_splits[0].expr() - left_half_shifted_expected_mle.expr());
+    let output_part_2 =
+        builder.add_sector(shifter_splits[1].expr() - right_half_shifted_expected_mle.expr());
 
     builder.set_output(&output_part_1);
     builder.set_output(&output_part_2);
@@ -35,12 +38,34 @@ fn build_circuit() -> Circuit<Fr> {
 }
 
 fn attach_data(mut circuit: Circuit<Fr>) -> ProvableCircuit<Fr> {
-    let data = [0, 0, 1, 0, 1, 1, 0, 1];
+    let data = [1, 2, 3, 4, 5, 6, 7, 8];
+    // We are shifting evaluations right by 1.
+    let left_half_shifted_expected_data = [0, 1, 2, 3];
+    let right_half_shifted_expected_data = [4, 5, 6, 7];
 
     let input_mle = MultilinearExtension::new(data.into_iter().map(Fr::from).collect_vec());
-    // dbg!(&input_mle);
+    let left_half_shifted_expected_mle = MultilinearExtension::new(
+        left_half_shifted_expected_data
+            .into_iter()
+            .map(Fr::from)
+            .collect_vec(),
+    );
+    let right_half_shifted_expected_mle = MultilinearExtension::new(
+        right_half_shifted_expected_data
+            .into_iter()
+            .map(Fr::from)
+            .collect_vec(),
+    );
 
-    circuit.set_input("All Inputs", input_mle);
+    circuit.set_input("Primary Input MLE", input_mle);
+    circuit.set_input(
+        "Left Half Shifted Expected MLE",
+        left_half_shifted_expected_mle,
+    );
+    circuit.set_input(
+        "Right Half Shifted Expected MLE",
+        right_half_shifted_expected_mle,
+    );
 
     circuit.finalize().unwrap()
 }
